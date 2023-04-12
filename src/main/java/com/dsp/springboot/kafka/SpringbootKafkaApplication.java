868777dc263320151fdd34dc10f4ff5acbaa50fb
sivan <sivananaintha.perumal@gmail.com>
2023-04-12 14:28:55 -0400

    initial project for kafka

---
+++ src/main/java/com/dsp/springboot/kafka/SpringbootKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.dsp.springboot.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootKafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootKafkaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dsp/springboot/kafka/SpringbootKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsp.springboot.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootKafkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
